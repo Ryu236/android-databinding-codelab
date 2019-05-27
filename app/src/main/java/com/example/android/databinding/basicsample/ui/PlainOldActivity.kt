a29c088b5fd8ab501bf01c2badf9d49205e1f99f
Ryu236 <ryu.adv.2360@gmail.com>
2019-05-27 23:00:56 +0900

    Change inflation and remove UI calls from activuty

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -27,10 +27,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.Popularity
 import com.example.android.databinding.basicsample.data.SimpleViewModel
+import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
 
 /**
  * Plain old activity with lots of problems to fix.
@@ -43,10 +45,10 @@ class PlainOldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.plain_activity)
+        val binding: PlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.plain_activity)
 
-        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateName()
+        binding.name = "Ryutaro"
+        binding.lastName = "Kobayashi"
         updateLikes()
     }
 
@@ -60,14 +62,6 @@ class PlainOldActivity : AppCompatActivity() {
     }
 
     /**
-     * So much findViewById! We'll fix that with Data Binding.
-     */
-    private fun updateName() {
-        findViewById<TextView>(R.id.plain_name).text = viewModel.name
-        findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
-    }
-
-    /**
      * This method has many problems:
      * - It's calling findViewById multiple times
      * - It has untestable logic
@@ -76,7 +70,7 @@ class PlainOldActivity : AppCompatActivity() {
     private fun updateLikes() {
         findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
         findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
+                (viewModel.likes * 100 / 5).coerceAtMost(100)
         val image = findViewById<ImageView>(R.id.imageView)
 
         val color = getAssociatedColor(viewModel.popularity, this)
@@ -89,7 +83,7 @@ class PlainOldActivity : AppCompatActivity() {
     private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
         return when (popularity) {
             Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
+                    intArrayOf(android.R.attr.colorForeground)
             ).getColor(0, 0x000000)
             Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
             Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
